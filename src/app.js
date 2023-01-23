351304c3f1c001fdf0c0e50fbf4e6fb990c84e30
mpmg-co <122822727+mpmg-co@users.noreply.github.com>
2023-01-23 04:50:03 +0300

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
-function formatDate(timestamp) {
+function formateDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let day = date.getDay();
-  return `${day} ${hours} ${minutes}`;
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -18,10 +33,11 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  dateElement.innerHTML = formateDate(response.data.time * 1000);
 }
 
 let apiKey = "5b7bb269ta60f5f8cboefe333f0784a8";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=5b7bb269ta60f5f8cboefe333f0784a8&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=5b7bb269ta60f5f8cboefe333f0784a8&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
